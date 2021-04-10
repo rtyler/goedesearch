71a8a934adaf5b1c9d1cc804c36bdb58d8a2a7a3
R. Tyler Croy <rtyler@brokenco.de>
2021-04-09 22:17:12 -0700

    Implement the filters
    These are very inefficient, but a start

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use flate2::read::GzDecoder;
 use serde::Deserialize;
 use std::fs::File;
 use std::path::PathBuf;
+use std::collections::HashMap;
 
 fn main() -> Result<(), std::io::Error>{
     println!("Loading data file..");
@@ -16,6 +17,114 @@ fn main() -> Result<(), std::io::Error>{
     Ok(())
 }
 
+/**
+ * A search index
+ */
+#[derive(Clone, Debug)]
+struct Index {
+    items: HashMap<u64, Article>,
+}
+
+mod Filters {
+    const STOPWORDS: &'static [&'static str] = &["the", "be", "to", "of", "and",
+    "a", "in", "that", "have", "i", "it", "for", "not", "on", "with", "he", "as",
+    "you", "do", "at", "this", "but", "his", "by", "from", "wikipedia"];
+
+    fn stems(tokens: Vec<String>) -> Vec<String> {
+        use rust_stemmers::{Algorithm, Stemmer};
+        // Create a stemmer for the english language
+        let en_stemmer = Stemmer::create(Algorithm::English);
+        tokens.iter().map(|token| {
+            en_stemmer.stem(token).to_string()
+        }).collect()
+    }
+
+    fn tokenize(text: &str) -> Vec<&str> {
+        text.split(' ').collect()
+    }
+
+    fn lowercase(tokens: Vec<&str>) -> Vec<String> {
+        tokens.iter().map(|t| t.to_lowercase()).collect()
+    }
+
+    fn punctuation(tokens: Vec<String>) -> Vec<String> {
+        tokens.iter().map(|token| {
+            token.chars()
+                .filter(|c| !c.is_ascii_punctuation())
+                .collect()
+        }).collect()
+    }
+
+    fn stopwords(tokens: Vec<String>) -> Vec<String> {
+        tokens.into_iter().filter(|token| {
+            ! STOPWORDS.contains(&token.as_str())
+        }).collect()
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_tokenize() {
+            let buf = "yo hello world";
+            assert_eq!(
+                vec!["yo", "hello", "world"],
+                tokenize(buf));
+        }
+
+        #[test]
+        fn test_lowercase() {
+            let tokens = vec!["HellO", "WORLd"];
+            assert_eq!(
+                vec!["hello", "world"],
+                lowercase(tokens));
+        }
+
+        #[test]
+        fn test_punctuation() {
+            let tokens = vec!["This,".to_string(), "isn't".to_string(), "great?".to_string()];
+            assert_eq!(
+                vec!["This", "isnt", "great"],
+                punctuation(tokens));
+        }
+
+        #[test]
+        fn test_stopwords() {
+            let tokens = vec!["i".to_string(),
+                                "am".to_string(),
+                                "the".to_string(),
+                                "walrus".to_string()];
+            assert_eq!(vec!["am", "walrus"], stopwords(tokens));
+        }
+
+        #[test]
+        fn test_stems() {
+            let tokens = vec!["help".to_string(),
+                                "fruitlessly".to_string(),
+                                "fruitless".to_string()];
+            assert_eq!(vec!["help", "fruitless", "fruitless"], stems(tokens));
+        }
+    }
+}
+
+impl Index {
+    fn new() -> Self {
+        Self {
+            items: HashMap::default(),
+        }
+    }
+
+    fn index_document(&mut self, article: Article) -> Result<(), std::io::Error> {
+        let id = article.id();
+        if ! self.items.contains_key(&id) {
+            // Should index
+        }
+
+        //  do something with tokens
+        Ok(())
+    }
+}
 
 /**
  * A wikipedia abstract data structure
